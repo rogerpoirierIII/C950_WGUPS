93c0e1838e1198b130d66a02c8d989aa9c0ef2c6
Roger Poirier <rpoiri2@wgu.edu>
2024-09-12 22:41:48 -0500

    adds distance CSV file

--- main.py
+++ main.py
@@ -5,16 +5,30 @@ from package import Package
 
 import csv
 
-package_hash = HashTable()
-with open("packages.csv", encoding="utf-8") as fp:
-    reader = csv.DictReader(fp,delimiter=",")
+def generate_package_Hash_Table(file):
+    hash_table = HashTable()
+    # Reads packages csv file
+    with open(file) as fp:
+        reader = csv.DictReader(fp)
 
+        # creates a new package object for each row in csv file
+        for row in reader:
+            package = Package(row['Package\nID'],row['Address'],row['City '],row['State'],row['Zip'],row['Delivery\nDeadline'],row['Weight\nKILO'])
+            hash_table.insert(package.id, package)
+        return hash_table
+
+distances = []
+with open("distances.csv") as fp:
+    reader = csv.DictReader(fp)
+    address = next(reader)[:]
     for row in reader:
 
-        # create a Package instance to store in the hash table
-        package = Package(row['Package\nID'],row['Address'],row['City '],row['State'],row['Zip'],row['Delivery\nDeadline'],row['Weight\nKILO'])
+        distances.append(row)
+    print(distances)
 
-        package_hash.insert(package.id, package)
 
-    print(package_hash.table)
-    print(package_hash.lookup('5').address)
+
+
+# package_table = generate_package_Hash_Table("packages.csv")
+# print(package_table.table)
+# print(package_table.lookup('5').address)
